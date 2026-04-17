25b29554d26f45d424bde30c3f23db53969d0a57
Zikirullo <zikirulloabdumannopov@gmail.com>
2026-04-17 13:54:35 +0900

    feat: Python system variables & special methon via dunder

--- run.py
+++ run.py
@@ -1,1 +1,14 @@
-print("Hello World!")
+# Dunder: __buitdins__, __init__.
+
+message = ("PYTHON: Everything is object!")
+print(message)
+
+result = type(message)
+print(result)
+
+'''In Python there are buindin tools:
+01.TYPES - int, float, str, list, dict
+02.FUNCTION - print(), len(), input(), type()
+03.CONSTANS - True, False, None 
+'''
+print(dir(__builtins__))
