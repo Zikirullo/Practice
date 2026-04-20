f7ededd1293b5be78e1063afb7de7b95d3c01e3c
Zikirullo <zikirulloabdumannopov@gmail.com>
2026-04-20 15:08:19 +0900

    feat: specific error handling system

--- objects.py
+++ objects.py
@@ -16,3 +16,22 @@ print(result1)
 
 result2 = ceil(98.7)
 print(result2)
+
+print("=== Error handling system ===")
+
+car_dict = dict(name="Toyota", year=2026, electric=True)
+
+try:
+    print("passed here")
+    change = car_dict.speed
+    result = car_dict["origin "]
+    print("result")
+except KeyError as err:
+    print("No data for origin was found, please try again", err)
+except AttributeError as err:
+    print("whatever you're asking there's no data for that")
+else:
+    print("Executec successfully without an error")
+finally:
+    print("Final closing logic")
+# else mantig'i qachonki try: da error bolmasa ishga tushadi. finally esa xar doim ishga tushadi, eng oxiridda.
