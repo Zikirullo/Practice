1f053aaced86dda595b6365809470f8f05795995
Zikirullo <zikirulloabdumannopov@gmail.com>
2026-04-20 16:51:06 +0900

    fix mitTask-B, fixed details

--- train.js
+++ train.js
@@ -1,27 +1,10 @@
-/*MITTASK-A
-Shunday 2 parametrli function tuzing, hamda birinchi parametrdagi letterni ikkinchi parametrdagi sozdan qatnashga sonini return qilishi kerak boladi.
- MASALAN countLetter("e", "engineer") 3ni return qiladi.
-*/
+/* B-TASK (NodeJS)
 
-console.log("Solution");
-
-function mitTaskA(letter, word) {
-  const splitWord = word.split("");
-  const filterWord = splitWord.filter((ele) => {
-    return ele === letter;
-  });
-  return filterWord.length;
-}
-
-console.log(mitTaskA("o", "toyota"));
-
-/* 
-B-TASK (NodeJS)
-
-Shunday function tuzing, u 1ta string parametrga ega bolsin, hamda osha stringda qatnashgan raqamlarni sonini bizga return qilsin.
+Savol: Shunday function tuzing, u 1ta string parametrga ega bolsin, hamda osha stringda qatnashgan raqamlarni sonini bizga return qilsin.
 MASALAN countDigits("ad2a54y79wet0sfgb9") 7ni return qiladi.
 */
 
+// Masalaning yechimi
 console.log("Solutuion2");
 
 function countDigits(str) {
@@ -33,3 +16,26 @@ function countDigits(str) {
 }
 
 console.log(countDigits("jfw923983sdf238dfh83r2"));
+
+// ===================================================
+
+/* A-TASK
+Savol: Shunday 2 parametrli function tuzing, hamda birinchi parametrdagi letterni ikkinchi parametrdagi sozdan qatnashga sonini return qilishi kerak boladi.
+ MASALAN countLetter("e", "engineer") 3ni return qiladi.
+*/
+
+// Masalaning yechimi
+
+// console.log("Solution");
+
+// function mitTaskA(letter, word) {
+//   const splitWord = word.split("");
+//   const filterWord = splitWord.filter((ele) => {
+//     return ele === letter;
+//   });
+//   return filterWord.length;
+// }
+
+// console.log(mitTaskA("o", "toyota"));
+
+// ============================================================
