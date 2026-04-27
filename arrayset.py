22f4fb5c118dead0092777c27b0bd66044a0c189
Zikirullo <zikirulloabdumannopov@gmail.com>
2026-04-27 17:08:09 +0900

    fix: learn specific operator with set

--- arrayset.py
+++ arrayset.py
@@ -12,3 +12,33 @@ numbers.pop()
 
 del numbers[0:2]
 print(numbers)
+
+print('=== SET ===')
+
+numbers = array("i", [1, 3, 5, 3, 7, 9, 8, 1, 6, 4, 2])
+new = set(numbers)
+print(new)
+
+new.add(200)
+print(new)
+
+new.add(3)
+print(new)
+
+print('=== Specific operators with SET ===')
+print("✅"*20)
+
+
+# |, &, -, ^
+
+a = {10, 20, 50}
+b = {20, 40}
+
+result1 = a | b  # union
+result2 = a & b  # intersection
+result3 = a - b  # difference
+result4 = a ^ b  # symmetric difference
+print("Union", result1)
+print("Intersection", result2)
+print("Difference", result3)
+print("Symmetric difference", result4)
