53500cb4bd12c612724852fc85d04995a25df1e1
Zikirullo <zikirulloabdumannopov@gmail.com>
2026-04-29 06:52:23 +0900

    feat: set and dictinary comprehension

---  Comprehension.py
+++  Comprehension.py
@@ -2,9 +2,9 @@ print('=== What is comprehension & list comprehension ===')
 # It acts like a spread operator of other stacks
 
 ''' Comprehension general syntax:
-01. * itarable
-02. <expression> for item in iterable
-03. <expression> for item in iterable <condition>
+A) * itarable
+B) <expression> for item in iterable
+C) <expression> for item in iterable <condition>
 '''
 # LIST COMPREHENSION
 numbers = [1, 2, 6, 4, 2, 10]
@@ -31,3 +31,21 @@ cars = [
 ]
 list_cars = [car[0] for car in cars if car[1] > 80]
 print("list_cars:", list_cars)
+
+print("✅"*20)
+print("")
+print("=== SET and DICTINARY comprehension ===")
+print("")
+
+numbs = [3, 44, 51, 4, 32, 4, 3, 5, 1, 4, 2, 4, 5, 44]
+set_numbers = {*numbs}
+print("SET_numbers:", set_numbers)
+print('')
+
+dict_people = {person[0]: person[1] for person in people}
+print("DICTINARY_people:", dict_people)
+print("")
+
+dict_people = {person[0] for person in people if person[1] <= 21}
+print("dict_people:", dict_people)
+print("")
