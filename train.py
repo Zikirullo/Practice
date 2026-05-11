b20b4dc15945136e4203bb7ce3ace3d9214fdacf
Zikirullo <zikirulloabdumannopov@gmail.com>
2026-05-11 14:32:57 +0900

    feat: MITTASK - K (Python)

--- train.py
+++ train.py
@@ -1,33 +1,47 @@
 
 '''
-I-TASK (PYTHON)
+K-TASK (PYTHON)
 
-Shunday function tuzing, unga string argument pass bolsin. Function ushbu agrumentdagi digitlarni yangi stringda return qilsin
-MASALAN: get_digits("m14i1t") return qiladi "141"
+Shunday function yozing, u string qabul qilsin va string ichidagi eng uzun sozni qaytarsin.
+MASALAN: find_longest("I come from Uzbekistan") return "Uzbekistan"
 '''
+print('=== Solution ===')
+
+
+def find_longest(s):
+    words = s.split()
+    longest = ""
+    for word in words:
+        if len(word) > len(longest):
+            longest = word
+    return longest
+
+
+print(find_longest("I come from Uzbekistan"))
+
+# I-TASK (PYTHON)
+
+# Shunday function tuzing, unga string argument pass bolsin. Function ushbu agrumentdagi digitlarni yangi stringda return qilsin
+# MASALAN: get_digits("m14i1t") return qiladi "141"
+
 # Masalaning yechimi
-print("=== Solution ===")
+# print("=== Solution ===")
 
+# def get_digits(digit):
+#     return ''.join(filter(str.isdigit, digit))
 
-def get_digits(digit):
-    return ''.join(filter(str.isdigit, digit))
+# print(get_digits("m14i321t"))
 
+# '''
+# G-TASK (PYTHON)
 
-print(get_digits("m14i321t"))
-
-
-'''
-G-TASK (PYTHON)
-
-Shunday function tuzingki unga integerlardan iborat array pass bolsin va function bizga osha arrayning eng katta qiymatiga tegishli birinchi indexni qaytarsin.
-MASALAN: get_highest_index([5, 21, 12, 21, 8]) return qiladi 1 sonini.
-'''
+# Shunday function tuzingki unga integerlardan iborat array pass bolsin va function bizga osha arrayning eng katta qiymatiga tegishli birinchi indexni qaytarsin.
+# MASALAN: get_highest_index([5, 21, 12, 21, 8]) return qiladi 1 sonini.
+# '''
 # Masalaning yechimi
-print("=== Solution ===")
+# print("=== Solution ===")
 
+# def task(arr):
+#     return arr.index(max(arr))
 
-def task(arr):
-    return arr.index(max(arr))
-
-
-print(task([5, 3, 2, 4, 2, 4, 4, 23]))
+# print(task([5, 3, 2, 4, 2, 4, 4, 23]))
