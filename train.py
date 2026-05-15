e429ba6b3a6adc205ec2e008acf8f6b5858c818e
Zikirullo <zikirulloabdumannopov@gmail.com>
2026-05-15 16:03:44 +0900

    feat: MITTASK - K (Python)

--- train.py
+++ train.py
@@ -1,5 +1,14 @@
 
 '''
+M-TASK (PYTHON)
+
+Shunday function yozing, u string qabul qilsin va string palindrom yani togri oqilganda ham, orqasidan oqilganda ham bir hil oqiladigan soz ekanligini aniqlab boolean qiymat qaytarsin.
+MASALAN: palindrom_check("dad") return True;  palindrom_check("son") return False;
+'''
+print("Solution")
+
+
+'''
 K-TASK (PYTHON)
 
 Shunday function yozing, u string qabul qilsin va string ichidagi eng uzun sozni qaytarsin.
@@ -8,16 +17,25 @@ MASALAN: find_longest("I come from Uzbekistan") return "Uzbekistan"
 print('=== Solution ===')
 
 
-def find_longest(s):
-    words = s.split()
-    longest = ""
-    for word in words:
-        if len(word) > len(longest):
-            longest = word
-    return longest
+def palindrom_check(word):
+    reversed_word = word[::-1]
+
+    return word == reversed_word
 
 
-print(find_longest("I come from Uzbekistan"))
+print(palindrom_check("dad"))
+print(palindrom_check("son"))
+
+# def find_longest(s):
+#     words = s.split()
+#     longest = ""
+#     for word in words:
+#         if len(word) > len(longest):
+#             longest = word
+#     return longest
+
+
+# print(find_longest("I come from Uzbekistan"))
 
 # I-TASK (PYTHON)
 
