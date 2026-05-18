8cd15212a6db5bd44b15fec4472a4245a477f995
Zikirullo <zikirulloabdumannopov@gmail.com>
2026-05-18 18:27:10 +0900

    feat: MITTASK - N (NodeJS) ;

--- train.js
+++ train.js
@@ -1,4 +1,24 @@
 /*
+N-TASK (NodeJS)
+
+Shunday function yozing, u raqamlardan tashkil topgan array qabul qilsin va array ichidagi har bir raqam uchun raqamni ozi va hamda osha raqamni kvadratidan tashkil topgan object hosil qilib, hosil bolgan objectlarni array ichida qaytarsin.
+MASALAN: getSquareNumbers([1, 2, 3]) return [{number: 1, square: 1}, {number: 2, square: 4}, {number: 3, square: 9}];
+*/
+
+console.log("Solution");
+
+function getSquareNumbers(arr) {
+  return arr.map((num) => {
+    return {
+      number: num,
+      square: num ** 2,
+    };
+  });
+}
+
+console.log(getSquareNumbers([1, 2, 3]));
+
+/*
 L-TASK
 Shunday function yozing, u string qabul qilsin va
 string ichidagi hamma sozlarni chappasiga yozib,
@@ -7,19 +27,19 @@ sozlar ketma-ketligini buzmasdan stringni qaytarsin.
 MASALAN:
 reverseSentence("we like coding!") => "ew ekil !gnidoc"
 */
-console.log("Solution");
+// console.log("Solution");
 
-function reverseSentence(str) {
-  const splitWords = str.split(" ");
+// function reverseSentence(str) {
+//   const splitWords = str.split(" ");
 
-  const reversedWords = splitWords.map((word) => {
-    return word.split("").reverse().join("");
-  });
+//   const reversedWords = splitWords.map((word) => {
+//     return word.split("").reverse().join("");
+//   });
 
-  return reversedWords.join(" ");
-}
+//   return reversedWords.join(" ");
+// }
 
-console.log(reverseSentence("we like coding!"));
+// console.log(reverseSentence("we like coding!"));
 
 /*
 J-TASK (NodeJS)
