d519f6943da8d81d45f17bc276c9f127ddaeb5b7
Zikirullo <zikirulloabdumannopov@gmail.com>
2026-05-20 04:41:51 +0900

    feat: MITTASK - G (python)

--- train.py
+++ train.py
@@ -1,11 +1,33 @@
 
 '''
+O-TASK (PYTHON)
+
+Shunday function yozing, u har xil valuelardan iborat array qabul qilsin va List ichidagi sonlar yigindisini hisoblab chiqqan javobni qaytarsin.
+MASALAN: calculate_summary([10, "10", {son: 10}, true, 35]) return 45
+'''
+print("Solution")
+
+
+def calculate_summary(arr):
+    total = 0
+
+    for x in arr:
+        if type(x) == int or type(x) == float:
+            total = total + x
+
+    return total
+
+
+result = calculate_summary([100, "10", {"son": 10}, True, 35])
+print(result)
+
+'''
 M-TASK (PYTHON)
 
 Shunday function yozing, u string qabul qilsin va string palindrom yani togri oqilganda ham, orqasidan oqilganda ham bir hil oqiladigan soz ekanligini aniqlab boolean qiymat qaytarsin.
 MASALAN: palindrom_check("dad") return True;  palindrom_check("son") return False;
 '''
-print("Solution")
+# print("Solution")
 
 
 '''
@@ -14,17 +36,17 @@ K-TASK (PYTHON)
 Shunday function yozing, u string qabul qilsin va string ichidagi eng uzun sozni qaytarsin.
 MASALAN: find_longest("I come from Uzbekistan") return "Uzbekistan"
 '''
-print('=== Solution ===')
+# print('=== Solution ===')
 
 
-def palindrom_check(word):
-    reversed_word = word[::-1]
+# def palindrom_check(word):
+#     reversed_word = word[::-1]
 
-    return word == reversed_word
+#     return word == reversed_word
 
 
-print(palindrom_check("dad"))
-print(palindrom_check("son"))
+# print(palindrom_check("dad"))
+# print(palindrom_check("son"))
 
 # def find_longest(s):
 #     words = s.split()
